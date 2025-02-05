7f7112a0d6674e66f2fdc0bfafd25a6fc34d075d
andyrews <126462930+andyrews@users.noreply.github.com>
2025-02-05 16:26:41 +0000

    Added Endpoints

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-from mylib.logic import wiki
+from mylib.logic import *
+from fastapi import FastAPI
 
-res = wiki()
+import uvicorn
 
-print(wiki())
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API. Call /search or /wiki"}
+
+@app.get("/search/{val}")
+async def search(val: str):
+    """Page to search in wiki"""
+    res = search_wiki(val)
+    return {"results": res}
+
+@app.get("/wiki/{name}")
+async def wiki_search(name: str):
+    """Get page of wiki"""
+    res = wiki(name)
+    return {"results": res}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
+
